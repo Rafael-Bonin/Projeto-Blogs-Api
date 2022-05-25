313ad93706e78e10b6e3a96fa820bee9cbeea759
Rafael <rafalyf11@gmail.com>
2022-05-25 18:56:29 -0300

    feat: requisito 11

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -1,11 +1,20 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+
+const PostCategory = (sequelize, DataTypes) => {
   const PostCategory = sequelize.define('PostCategory', {
     postId: DataTypes.INTEGER,
     categoryId: DataTypes.INTEGER
-  }, {timestamps: false})
+  }, {timestamps: false});
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+       as: 'categories', foreignKey: 'postId', through: PostCategory, otherKey: 'categoryId' });
+
+    models.Category.belongsToMany(models.BlogPost, {
+       as: 'categories', foreignKey: 'categoryId', through: PostCategory, otherKey: 'postId' });
+  }
+
   return PostCategory;
 };
+
+module.exports = PostCategory;
